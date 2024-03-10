968d9efecfde82328811be6bc8141938a09cfee1
Lovepreet Singh <luvprits33@gmail.com>
2024-03-10 16:41:12 +0530

    Created About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-5 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Welcome to our Movie Database Website! We are a team of passionate movie enthusiasts who have come together to
+            create a one-stop destination for all your movie-related needs
+        </p> 
+        <p>
+            Our website is designed to provide you with a comprehensive database of movies from all around the world, along with
+            the latest news, reviews and trailers. Our movie database is constantly updated with new releases, ensuring that you 
+            have access to latest and greatest in the world of cinema.
+        </p> 
+        <p>
+            In addition to our extensive movie database, we also offer a platform for movie lovers to connect and share their thoughts
+            on the latest releases.
+        </p> 
+    </div>
+  )
+}
